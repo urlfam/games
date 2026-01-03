4c638432b40d2496bc57bb943b17b14908f26c46
User <user@Users-MBP.lan>
2026-01-03 14:03:38 +0100

    Apply performance optimizations: AVIF, Gzip, Nginx Cache

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60,
     remotePatterns: [
       {
         protocol: 'https',
